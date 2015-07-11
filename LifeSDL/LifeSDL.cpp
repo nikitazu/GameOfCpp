ff6b7f6f7f23213fadfffd65aac4101f9123fc78
Nikita Zuev <nikitazu@gmail.com>
2015-07-11 15:39:58 +0300

    can draw rectangle

--- LifeSDL/LifeSDL.cpp
+++ LifeSDL/LifeSDL.cpp
@@ -7,9 +7,11 @@ const int SCREEN_WIDTH = 1024;
 const int SCREEN_HEIGHT = 768;
 const int SCREEN_BPP = 32;
 
-int _tmain(int argc, _TCHAR* argv[])
-{
+void draw(SDL_Surface &screen, SDL_Surface &canvas);
+
+int _tmain(int argc, _TCHAR* argv[]) {
     SDL_Surface* screen = NULL;
+    SDL_Surface* canvas = NULL;
     SDL_Event event;
     bool quit = false;
 
@@ -23,11 +25,22 @@ int _tmain(int argc, _TCHAR* argv[])
         return EXIT_FAILURE;
     }
 
+    if (NULL == (canvas = SDL_CreateRGBSurface(0, SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, 0, 0, 0, 0))) {
+        printf("SDL create canvas failed\n");
+        return EXIT_FAILURE;
+    }
+
     SDL_WM_SetCaption("Game of Life", NULL);
 
     while (!quit) {
         while (SDL_PollEvent(&event)) {
             quit = (event.type == SDL_QUIT || event.type == SDL_KEYDOWN);
+        }
+
+        draw(*screen, *canvas);
+        if (-1 == SDL_Flip(screen)) {
+            printf("SDL flip screen failed\n");
+            return EXIT_FAILURE;
         }
     }
 
@@ -35,3 +48,12 @@ int _tmain(int argc, _TCHAR* argv[])
     return EXIT_SUCCESS;
 }
 
+void draw(SDL_Surface &screen, SDL_Surface &canvas) {
+    SDL_Rect rect;
+    rect.x = 10;
+    rect.y = 10;
+    rect.w = 100;
+    rect.h = 100;
+    SDL_FillRect(&screen, &screen.clip_rect, SDL_MapRGB(screen.format, 0xFF, 0xFF, 0xFF));
+    SDL_BlitSurface(&canvas, &rect, &screen, &rect);
+}

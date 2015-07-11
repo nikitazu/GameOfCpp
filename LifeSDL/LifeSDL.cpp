68fdcd3fea6db2bfb551f20dac233bb382114665
Nikita Zuev <nikitazu@gmail.com>
2015-07-11 15:16:18 +0300

    event loop

--- LifeSDL/LifeSDL.cpp
+++ LifeSDL/LifeSDL.cpp
@@ -5,22 +5,31 @@
 
 const int SCREEN_WIDTH = 1024;
 const int SCREEN_HEIGHT = 768;
+const int SCREEN_BPP = 32;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
     SDL_Surface* screen = NULL;
+    SDL_Event event;
+    bool quit = false;
 
     if (-1 == SDL_Init(SDL_INIT_EVENTTHREAD)) {
         printf("SDL init failed\n");
         return EXIT_FAILURE;
     }
 
-    if (NULL == (screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32, SDL_SWSURFACE))) {
+    if (NULL == (screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE))) {
         printf("SDL set video mode failed\n");
         return EXIT_FAILURE;
     }
 
-    SDL_Delay(2000);
+    SDL_WM_SetCaption("Game of Life", NULL);
+
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            quit = (event.type == SDL_QUIT || event.type == SDL_KEYDOWN);
+        }
+    }
 
     SDL_Quit();
     return EXIT_SUCCESS;
